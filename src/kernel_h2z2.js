fa8c4d15ec6a169850d0f14e74a24da666f86fd0
xzcv232 <ftjvi62k6g@mail10p.com>
2026-01-02 10:43:56 +0700

    test: add unit tests for worker node

--- src/kernel_h2z2.js
+++ src/kernel_h2z2.js
@@ -3,3 +3,8 @@ module.exports = config;
 
 // Update 757e
 function processData(d) { return d.map(x => x * 2); }
+
+// Update mxwo
+def run_task_mxwo():
+    """Subsystem version 4787"""
+    return "mxwo".upper()
